0291db01db4892504159cfeacffb58d7ac5c57ba
jc16g22 <jc16g22@soton.ac.uk>
2025-05-28 23:40:34 +0100

    Altered background colour

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,7 +6,7 @@ export default function Layout() {
     return (
         <div className="flex flex-col min-h-screen">
             <Header />
-            <main className="flex-1 bg-gradient-to-b from-blue-900 to-blue-600 pt-22 pb-12 px-4">
+            <main className="flex-1 bg-gradient-to-b from-blue-950 to-blue-800 pt-22 pb-12 px-4">
                 <Outlet />
             </main>
             <Footer />

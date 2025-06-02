f7258e4516e9e69223caa9c4c9276a7e9abb6051
jc16g22 <jc16g22@soton.ac.uk>
2025-06-02 19:18:42 +0100

    Fixed two navigation issues: scroll resets on page change and refresh routes to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop"
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
@@ -7,14 +8,17 @@ import Competencies from "./pages/Competencies";
 function App() {
 
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/competencies" element={<Competencies />} />
-        <Route path="*" element={<h1>404 - Page Not Found</h1>} />
-      </Route>
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/competencies" element={<Competencies />} />
+          <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 

da12adc339d41ba00bc382e3f47316fa9edec3e5
jc16g22 <jc16g22@soton.ac.uk>
2025-06-03 12:14:41 +0100

    Added login and register forms

--- src/pages/blog/Login.jsx
+++ src/pages/blog/Login.jsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Login() {
     return (
-        <h1>Login</h1>
+        <div className="p-4 mt-16 max-w-3xl mx-auto text-center bg-gray-300/90 text-gray-800 rounded">
+            <h2 className="text-3xl font-bold mb-4">Login</h2>
+            <input placeholder="Username or Email Address" className="w-full mb-2 p-2 border bg-gray-200" />
+            <input type="password" placeholder="Password" className="w-full mb-2 p-2 border bg-gray-200" />
+            <button className="bg-purple-800 text-white text-xl px-8 py-1 mt-4 rounded shadow">Confirm</button>
+            <p className="text-md pt-8 text-gray-500">Don't have an account? Register <Link to="/blog/register" className="underline">here</Link>.</p>
+        </div>
     );
 }
--- src/pages/blog/Register.jsx
+++ src/pages/blog/Register.jsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+
 export default function Register() {
     return (
-        <h1>Register</h1>
+        <div className="p-4 mt-16 max-w-3xl mx-auto text-center bg-gray-300/90 text-gray-800 rounded">
+            <h2 className="text-3xl font-bold mb-4">Register</h2>
+            <input placeholder="Email Address" className="w-full mb-2 p-2 border bg-gray-200" />
+            <input placeholder="Username" className="w-full mb-2 p-2 border bg-gray-200" />
+            <input type="password" placeholder="Password" className="w-full mb-2 p-2 border bg-gray-200 mt-8" />
+            <input type="password" placeholder="Confirm Password" className="w-full mb-2 p-2 border bg-gray-200" />
+            <button className="bg-purple-800 text-white text-xl px-8 py-1 mt-4 rounded shadow">Confirm</button>
+            <p className="text-md pt-8 text-gray-500">Already have an account? Log in <Link to="/blog/login" className="underline">here</Link>.</p>
+        </div>
     );
 }

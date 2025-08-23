062caf4aeadcfc2104a28dd79043d29508d9ff5d
Jamming17 <jackchiplin@outlook.com>
2025-08-23 11:56:16 +0100

    Added TypeScript to Competencies and Jamming Arcade to projects

--- src/components/CompetencyCategory.jsx
+++ src/components/CompetencyCategory.jsx
@@ -5,7 +5,7 @@ export default function CompetencyCategory({ title, tools }) {
             <h2 className="mb-4 py-1 bg-gray-300/90 text-3xl text-gray-800 text-center font-bold shadow rounded">{title}</h2>
             <div className="flex flex-wrap gap-4 text-center justify-center">
                 {tools.map((tool, index) => (
-                    <div key={index} className="w-34 p-2 gap-2 bg-gray-300/90 text-gray-800 shadow rounded">
+                    <div key={index} className={`w-34 p-2 gap-2 bg-gray-300/90 text-gray-800 shadow rounded ${tool.gold && "border-4 border-amber-400"}`}>
                         <img 
                             src={tool.logo}
                             alt={`${tool.name} logo`}
--- src/pages/Competencies.jsx
+++ src/pages/Competencies.jsx
@@ -5,6 +5,7 @@ import javaLogo from "../assets/logos/java.png";
 import csharpLogo from "../assets/logos/csharp.png";
 import javascriptLogo from "../assets/logos/javascript.png";
 import haskellLogo from "../assets/logos/haskell.png";
+import typescriptLogo from "../assets/logos/typescript.png";
 
 import htmlcssLogo from "../assets/logos/htmlcss.png";
 import reactLogo from "../assets/logos/react.png";
@@ -45,60 +46,62 @@ import pythonunittestLogo from "../assets/logos/pythonunittest.png";
 
 export default function Competencies() {
     const programmingLanguages = [
-        { name: "Python", logo: pythonLogo },
-        { name: "Java", logo: javaLogo },
-        { name: "C#", logo: csharpLogo },
-        { name: "JavaScript", logo: javascriptLogo },
-        { name: "Haskell", logo: haskellLogo }
+        { name: "Java", logo: javaLogo, gold: true },
+        { name: "JavaScript", logo: javascriptLogo, gold: true },
+        { name: "TypeScript", logo: typescriptLogo, gold: false },
+        { name: "Python", logo: pythonLogo, gold: false },
+        { name: "C#", logo: csharpLogo, gold: false },
+        { name: "Haskell", logo: haskellLogo, gold: false }
     ];
     const frontendTechnologies = [
-        { name: "CSS + HTML", logo: htmlcssLogo },
-        { name: "React", logo: reactLogo },
-        { name: "Vue.js", logo: vuejsLogo },
-        { name: "Tailwind CSS", logo: tailwindcssLogo },
-        { name: "Bootstrap", logo: bootstrapLogo },
-        { name: "Vite", logo: viteLogo }
+        { name: "CSS + HTML", logo: htmlcssLogo, gold: true },
+        { name: "React", logo: reactLogo, gold: true },
+        { name: "Vue.js", logo: vuejsLogo, gold: false },
+        { name: "Tailwind CSS", logo: tailwindcssLogo, gold: true },
+        { name: "Bootstrap", logo: bootstrapLogo, gold: false },
+        { name: "Vite", logo: viteLogo, gold: false }
     ];
     const backendTechnologies = [
-        { name: "Node.js", logo: nodejsLogo },
-        { name: "Express.js", logo: expressjsLogo },
-        { name: "Azure Python Functions", logo: azurepythonfunctionappLogo },
-        { name: "CosmosDB", logo: cosmosdbLogo },
-        { name: "PostgreSQL", logo: postgresqlLogo}
+        { name: "Node.js", logo: nodejsLogo, gold: true },
+        { name: "Express.js", logo: expressjsLogo, gold: false },
+        { name: "Azure Python Functions", logo: azurepythonfunctionappLogo, gold: false },
+        { name: "CosmosDB", logo: cosmosdbLogo, gold: false },
+        { name: "PostgreSQL", logo: postgresqlLogo, gold: false }
     ];
     const cloudAndDeployment = [
-        { name: "Microsoft Azure", logo: azureLogo },
-        { name: "Google App Engine", logo: gaeLogo },
-        { name: "Netlify", logo: netlifyLogo },
-        { name: "Linux Server", logo: linuxserver }
+        { name: "Microsoft Azure", logo: azureLogo, gold: false },
+        { name: "Google App Engine", logo: gaeLogo, gold: false },
+        { name: "Netlify", logo: netlifyLogo, gold: false },
+        { name: "Linux Server", logo: linuxserver, gold: false }
     ];
     const cybersecurity = [
-        { name: "Burp Suite", logo: burpsuiteLogo },
-        { name: "Wireshark", logo: wiresharkLogo },
-        { name: "Web Penetration Testing", logo: xssLogo },
-        { name: "Cyber Kill Chain Analysis", logo: killchainLogo }
+        { name: "Burp Suite", logo: burpsuiteLogo, gold: false },
+        { name: "Wireshark", logo: wiresharkLogo, gold: false },
+        { name: "Web Penetration Testing", logo: xssLogo, gold: false },
+        { name: "Cyber Kill Chain Analysis", logo: killchainLogo, gold: false }
     ];
     const aiAndData = [
-        { name: "Numpy", logo: numpyLogo },
-        { name: "Pandas", logo: pandasLogo },
-        { name: "Tensorflow", logo: tensorflowLogo },
-        { name: "Scikit-Learn", logo: scikitlearnLogo },
-        { name: "OpenAI API", logo: openaiLogo }
+        { name: "Numpy", logo: numpyLogo, gold: false },
+        { name: "Pandas", logo: pandasLogo, gold: false },
+        { name: "Tensorflow", logo: tensorflowLogo, gold: false },
+        { name: "Scikit-Learn", logo: scikitlearnLogo, gold: false },
+        { name: "OpenAI API", logo: openaiLogo, gold: false }
     ];
     const developmentMethodologies = [
-        { name: "SCRUM", logo: scrumLogo },
-        { name: "Agile Development", logo: agileLogo },
-        { name: "UML", logo: umlLogo }
+        { name: "SCRUM", logo: scrumLogo, gold: false },
+        { name: "Agile Development", logo: agileLogo, gold: false },
+        { name: "UML", logo: umlLogo, gold: false }
     ];
     const testing = [
-        { name: "JUnit", logo: junitLogo },
-        { name: "Python unittest", logo: pythonunittestLogo }
+        { name: "JUnit", logo: junitLogo, gold: false },
+        { name: "Python unittest", logo: pythonunittestLogo, gold: false }
     ];
     return (
         <div className="prose max-w-5xl mx-auto">
             <h1 className="text-4xl font-bold mb-4 mt-6 md:mt-0">Competencies</h1>
             <p className="text-lg mb-10">
                 Below is a list of programming languages, software, frameworks, libraries and other tools that I have learned and practiced as a computer scientist. I have of course used more tools than just those listed below, but I have chosen to list only tools that I either know well now or would be able to get back into comfortably quickly.<br className="mb-6" />
+                Tools that I have the most experience with are highlighted in <span className="text-amber-400 font-bold">Gold</span>.<br className="mb-6" />
                 If you require more information about my skill in a particular area, feel free to contact me on one of the platforms listed in the footer of the page!
             </p>
             <CompetencyCategory title="Programming Languages" tools={programmingLanguages} />
